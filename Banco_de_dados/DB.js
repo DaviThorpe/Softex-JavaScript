922a56a923ad1b6a4da542a7cfe36326ede7c700
Davi Thorpe <davithorpe2@gmail.com>
2022-08-29 20:34:13 -0300

    att

---
+++ Banco_de_dados/DB.js
@@ -0,0 +1,21 @@
+// Implemente e trate uma conexão com o seu banco de dados, 
+// usando JavaScript. Caso a conexão com o banco tenha sucesso, 
+// ele deve retornar uma frase positiva. Se isso não ocorrer, 
+// retorne uma frase informando o erro.
+
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize ('DB', 'root', '',
+{host: "localhost",
+    dialect:'mysql'
+});
+
+sequelize.authenticate()
+.then(() => {
+	console.log('Conexão com o banco de dados realizada com sucesso')
+}) 
+.catch(() => {
+	console.log('Erro ao conectar com o banco de dados')
+}); 
+
+module.exports = sequelize;
